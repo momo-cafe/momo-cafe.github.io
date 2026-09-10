a11d67784092162a45084e13e57891132b847bf3
Mansur Nurmukhambetov <nomomon@users.noreply.github.com>
2026-09-10 16:03:17 +0500

    style: bump the header wordmark up a size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import wordmark from '@/assets/brand/momo-wordmark.svg?raw'
 export default function Header() {
   return (
     <header id="top" class="absolute top-0 left-0 z-10 px-6 py-6">
-      <a href="#top" class="inline-block w-24 text-white drop-shadow-md" innerHTML={wordmark} />
+      <a href="#top" class="inline-block w-28 text-white drop-shadow-md" innerHTML={wordmark} />
     </header>
   )
 }
